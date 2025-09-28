b7f765811fd5ca2cc00c23435ccbb3de42a4bc37
dyq <daiy@njust.edu.cn>
2025-09-28 23:22:15 +0900

    init

--- args.py
+++ args.py
@@ -9,44 +9,39 @@ def parse_train_opt():
     parser.add_argument("--exp_name", default="exp", help="save to project/name")
     
     ### dataset ###
-    parser.add_argument("--data_path", type=str, default="/opt/data/private/project3-Dancepartner/0-dataset/dataProcess/", help="raw data path") # 含 train/test 文件夹的 dir, 不要改
+    parser.add_argument("--data_path", type=str, 
+            default="./data/AIOZ_Dataset/", 
+            help="Path to raw dataset folder (must contain train/test subfolders). Do not modify unless necessary.")
     parser.add_argument("--processed_data_dir",type=str, 
-                        default="/opt/data/private/IJCV/opensource/EDGE/data/dataset_backups/", help="Dataset backup path") # full 3-dancer cache 路径
-    parser.add_argument("--batch_size", type=int, default=37, help="batch size") # layer-8, 4-gpu | 注意 bs 要大于20, 因为 test//10, 然后 rander_cont == 2
-    # parser.add_argument("--batch_size", type=int, default=65, help="batch size") # layer-8, 6-gpu
+                        default="./data/dataset_backups/", help="Dataset backup path") 
+    parser.add_argument("--batch_size", type=int, default=37) 
     parser.add_argument("--window_size", type=int, default=150, help="window size")
-        # change dancer_num
+    parser.add_argument( 
+        "--force_reload", default = False, action="store_true", help="Force reprocessing of the dataset, ignoring cached data."
+    )
+
+    parser.add_argument("--no_cache", action="store_true", default = False, help="Disable dataset caching; always reload from disk.")
+        
     parser.add_argument(
-        "--required_dancer_num", type = int, default=4, help="don't reuse / cache loaded dataset"
+        "--required_dancer_num", type = int, default=4, help="Number of dancers required in each sample."
     )
-    parser.add_argument( #dancer_num
-        "--split_file", type = str, 
-        default="/opt/data/private/IJCV/opensource/TCDiffpp_debug/data/dancernum_split/split_files/split_dancerNum_4.txt", help="don't reuse / cache loaded dataset"
-    )
-
-
-    parser.add_argument( # 强制 reload, 不适用 backup, False | 4 dump 了也是寄, 但是处理的很快
-        "--force_reload", default = False, action="store_true", help="force reloads the datasets" 
-    )
-    parser.add_argument("--no_cache", action="store_true", default = False, help="don't reuse / cache loaded dataset")
 
 
     ### Out Result ###
     parser.add_argument(
-        "--render_dir", type=str, default="./renders/", help="Sample render path" # 
+        "--vis_fk_out", type=str, default="./fk_out4Vis", help="Path to save FK outputs for visualization."
     )
     parser.add_argument(
-        "--wandb_pj_name", type=str, default="DanceDecoder-Modulation4", help="project name"
+        "--render_dir", type=str, default="./renders/", help="Path to save rendered sample videos."
     )
     parser.add_argument(
-        "--vis_fk_out", type=str, default="./fk_out4Vis", help="project name"
+        "--wandb_pj_name", type=str, default="TCDiffpp", help="Project name for Weights & Biases tracking."
     )
 
     ### Training ###
-    parser.add_argument( "--learning-rate", type=float, default = 0.0004, help="learning rate") # 52 bs-4gpu | 这个数量级大概一天能初步收敛
-    parser.add_argument("--epochs", type=int, default=8000)
-    parser.add_argument("--use_ssm", type=bool, default=False)
-    # parser.add_argument("--finetuning_epochs", type=int, default=2000) # Until Coveraged
+    parser.add_argument( "--learning-rate", type=float, default = 0.00005, help="learning rate") 
+    parser.add_argument("--epochs", type=int, default=10000)  # Until Coveraged
+    parser.add_argument("--use_ssm", type=bool, default=True)
     parser.add_argument(
         "--save_interval",
         type=int,
@@ -60,8 +55,6 @@ def parse_train_opt():
         help="trained checkpoint path (optional)"
     )
 
-    parser.add_argument("--mode", default = "train", choices=["train", "val"])
-
     opt = parser.parse_args()
     return opt
 
@@ -69,19 +62,12 @@ def parse_train_opt():
 def parse_test_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument("--required_dancer_num", type=int, default=4, help="traj stride")
-    parser.add_argument(
-        "--genre", 
-        type=str, 
-        default="Reggae", 
-        choices=["Indian", "Electronic", "Pop", "Rap", "RB", "Reggae", "Latin"],
-        help="Select the music genre."
-    )
 
     ### DanceDecoder model ###
     parser.add_argument(
         "--dance_checkpoint", type=str, 
-        default="/opt/data/private/IJCV/AblationStudy/TCDiffpp_fpdpesmfa/runs/train/exp4/weights/train-7000.pt",
-        # default="", 
+        # default="./runs/train/exp/weights/train-8000.pt",
+        default="", 
         help="checkpoint"
     )
     parser.add_argument(
@@ -94,16 +80,14 @@ def parse_test_opt():
 
     ### Out ###
     parser.add_argument(
-        "--render_dir", type=str, default="./renders/test/Dance/", help="Sample render path"
+        "--render_dir", type=str, default="./renders/test/LongDance/", help="Sample render path"
     )
-    parser.add_argument("--render_traj_dir", type=str, default="./renders/test/traj_renders/", help="traj render dir")
     parser.add_argument(
         "--save_motions", action="store_true", default= True, help="Saves the motions for evaluation"
     )
     parser.add_argument(
         "--motion_save_dir",
         type=str,
-        # default="./eval/motions",
         default = "./fk_out",
         help="Where to save the motions",
     )
@@ -111,42 +95,33 @@ def parse_test_opt():
     ### Slice Data ###
     parser.add_argument(
         "--cache_features",
-        default = True, # 保存 test 所需的数据
+        default = True, 
         action="store_true",
         help="Save the extracted features for later reuse",
     )
     parser.add_argument(
         "--use_cached_features",
-        default = True, # 还没处理的时候用 False, 只用用 True
+        default = True, 
         action="store_true",
         help="Use precomputed features instead of music folder",
     )
     parser.add_argument(
         "--feature_cache_dir",
         type=str,
-        default="/opt/data/private/project3-Dancepartner/0-dataset/Processed_data/cache_npy_wav/cache/all/", # all
-        # default = "/opt/data/private/project3-Dancepartner/temp_dir/cache_npy_wav/cache/train/", # setting:step(2/4) | 每次运行, 把成对的 wav 和 test 装到这里
+        default="./data/Processed_data/cache_npy_wav/cache/all/", 
         help="Where to save/load the features",
     )
     parser.add_argument(
         "--music_dir",
         type=str,
-        # default="/opt/data/private/dataset/AIOZ-dataset/musics", # all
-        # default="/opt/data/private/IJCV/opensource/_wildmusic/indian", # wild 
-        default = "/opt/data/private/project3-Dancepartner/temp_dir/cache_npy_wav/wav_debug/train", # # debug | setting:step(1/4) | 用于测试test的, 输入的未裁剪原始音频 .wav,
+        default="./data/AIOZ-dataset/musics", 
         help="folder containing input music",
     )
     parser.add_argument(
         "--music_process_dir",
         type=str,
-        default="/opt/data/private/project3-Dancepartner/0-dataset/Processed_data/cache_npy_wav",      
-        help="folder containing input music",
-    )
-    parser.add_argument(
-        "--seed_motion_pth",
-        type=str,
-        default="/opt/data/private/dataset/AIOZ-dataset/motions_smpl/",      
-        help="folder containing input music",
+        default="./data/Processed_data/cache_npy_wav",      
+        help="folder containing processed input music",
     )
     opt = parser.parse_args()
     return opt
